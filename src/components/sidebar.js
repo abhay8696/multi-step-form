6e2a4994f9a386a9607b8502817ada4a627e93d5
abhay8696 <abhaykamble1346@gmail.com>
2023-02-22 19:34:24 +0530

    reduced entire form size

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import '../styles/sidebar.css';
 
+import dummyImg from '../assets/images/bg-sidebar-desktop.svg';
+
 const Sidebar = () => {
+
+    //funtions
+    const
+    displayItems = ()=> {
+        const texts = ['', 'YOUR INFO', 'SELECT PLAN', 'ADD_ONS', 'SUMMARY'];
+        let arr = [];
+        for(let i=1; i<texts.length; i++){
+            arr.push(
+                <div className='sidebarItem'>
+                    <div className='sidebarBullet selected'>{i}</div>
+                    <div className='sidebarTexts'>
+                        <span className='sidebarText1 stepSpan'>STEP {i}</span>
+                        <span className='sidebarText2 itemName'>{texts[i]}</span>
+                    </div>
+                </div>
+            )
+        }
+
+        return arr;
+    }
+
     return (
         <div className='sidebar'>
-            
+            <div className='sidebarContainer'>
+                { displayItems() }
+            </div>
+            <img src={dummyImg} className='dummyImg'/>
         </div>
     );
 };
